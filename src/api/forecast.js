86d7533381dc727790a3c96f24edb2c08f1321b2
Surya <suryatejareddy222@gmail.com>
2024-05-24 15:41:00 +0530

    use https in api

--- src/api/forecast.js
+++ src/api/forecast.js
@@ -1,6 +1,6 @@
 async function fetch10DayForecast(locationKey, key) {
   const response = await fetch(
-    `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=${key}&details=true&metric=true`,
+    `https://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=${key}&details=true&metric=true`,
   );
   if (!response.ok) {
     throw new Error('Failed to fetch forecast');
--- src/api/weather.js
+++ src/api/weather.js
@@ -1,6 +1,6 @@
 async function getCurrentConditions(id, key) {
   const response = await fetch(
-    `http://dataservice.accuweather.com/currentconditions/v1/${id}?apikey=${key}&details=true`
+    `https://dataservice.accuweather.com/currentconditions/v1/${id}?apikey=${key}&details=true`
   );
 
   if (!response.ok) {
